5b3a8fc69a439a5f5d80eaf3dbd888d8c835e3fa
Yonas <yonasgeb09@gmail.com>
2026-06-17 19:10:51 +0300

    uploads from local storage to Cloudinary

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ import seedAdmin from "./seeds/adminSeeder.js";
 connectDB();
 
 const app = express();
-
+console.log("ENV CHECK (Render):", {
+  CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  KEY_EXISTS: !!process.env.CLOUDINARY_API_KEY,
+  SECRET_EXISTS: !!process.env.CLOUDINARY_API_SECRET,
+});
 /* =========================
    SAFE SEED (RUN ONCE)
    ========================= */
